64236942c1a76421227e26f9e95668ab0642d5ef
Suraj Prasad <suraj.suryn@gmail.com>
2023-06-25 09:08:17 +0530

    Print x^n (with stack height = logn)

---
+++ Recursion/lognPowerOfNumber.java
@@ -0,0 +1,26 @@
+package Recursion;
+
+import java.util.Scanner;
+
+public class lognPowerOfNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter base and Exponenet : ");
+        int n=sc.nextInt(),p=sc.nextInt();
+        int ans=powerofnumber(n, p);
+        System.out.print(n+" to the power "+p+ " = "+ans);
+        
+
+        sc.close();
+    }
+
+    private static int powerofnumber(int n, int p) {
+        if(p==0)
+            return 1;
+        else if (p%2==0) {
+            return powerofnumber(n, p/2)*powerofnumber(n, p/2);
+        }else{
+            return powerofnumber(n, p/2)*powerofnumber(n, p/2)*n;
+        }
+    }
+}
